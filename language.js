fc021e7ad98301af6be84c7acf8c0fb8c66f5219
David Steinkopff <d.steinkopff@tablonautix.com>
2014-09-16 09:56:01 +0200

    custom lang

---
+++ language.js
@@ -0,0 +1,15 @@
+jQuery(function () {
+  'use strict';
+  var language = navigator.userLanguage || navigator.language,
+    allLanguages = ['de', 'en'], style = '';
+  language = language.substring(0, 2).toLowerCase();
+  if (allLanguages.indexOf(language) === -1) {
+    language = 'en';
+  }
+  allLanguages.forEach(function (lang) {
+    if (language !== lang) {
+      style += ':lang(' + lang + ') { display:none; }';
+    }
+  });
+  $('<style/>').text(style).appendTo('head');
+});
